f2c27c18ad2f40832e93cc06bb8ded0eba11e854
adambl4 <dreadambl4@gmail.com>
2020-04-22 20:43:51 +0300

    Adapt rsync source dir for Cygwin environment

--- plugin/src/main/kotlin/utils.kt
+++ plugin/src/main/kotlin/utils.kt
@@ -1,4 +1,5 @@
 import groovy.lang.Closure
+import org.apache.tools.ant.taskdefs.condition.Os
 import org.gradle.BuildAdapter
 import org.gradle.BuildResult
 import org.gradle.api.Project
@@ -62,7 +63,7 @@ fun prettyTime(timeInMs: Long): String {
 class MirakleException(message: String) : RuntimeException(message)
 
 inline fun <reified T : Task> Project.task(name: String, noinline configuration: T.() -> Unit) =
-        tasks.create(name, T::class.java, configuration)!!
+        tasks.create(name, T::class.java, configuration)
 
 
 //void buildFinished(Action<? super BuildResult> action) is available since 3.4
@@ -88,3 +89,14 @@ val Task.services: ServiceRegistry get() {
     field.isAccessible = true
     return field.get(this) as ServiceRegistry
 }
+
+/*
+* On Windows rsync is used only under Cygwin environment
+* and classical Windows path "C:\" must be replaced by "/cygdrive/c/"
+* */
+fun fixDirForRsync(dir: String) =
+        if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+            val windowsDisk = dir.first().toLowerCase()
+            val path = dir.substringAfter(":\\")
+            "/cygdrive/$windowsDisk/$path"
+        } else dir
