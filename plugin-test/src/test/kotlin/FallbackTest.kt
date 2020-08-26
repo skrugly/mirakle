a39bbc437fd466baa607613292fa5f0043645253
Adambl4 <18431607+Adambl4@users.noreply.github.com>
2020-08-26 18:22:13 +0300

    Fail build when fallback enabled and remote execution fails (#48)

--- plugin-test/src/test/kotlin/FallbackTest.kt
+++ plugin-test/src/test/kotlin/FallbackTest.kt
@@ -3,6 +3,7 @@ import org.gradle.testkit.runner.GradleRunner
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
 import java.io.FileWriter
 import java.io.PrintWriter
 
@@ -50,6 +51,45 @@ object FallbackTest : Spek({
                 it("executeOnRemote and downloadFromRemote should be skipped") {
                     testResult.assertTaskSkipped("executeOnRemote")
                     testResult.assertTaskSkipped("downloadFromRemote")
+                }
+            }
+
+            describe("successfully configured mirakle with fallback") {
+                beforeEachTest {
+                    folder.newFile("mirakle_init.gradle")
+                            .outputStream()
+                            .let(::PrintWriter)
+                            .use {
+                                it.append(MIRAKLE_INIT_WITH_FOLDER(folder.root.canonicalPath).replace("fallback false", "fallback true"))
+                            }
+                }
+
+                val gradleRunner by memoized {
+                    GradleRunner.create()
+                            .withProjectDir(folder.root)
+                            .withGradleVersion(gradleVersion)
+                            .forwardOutput()
+                            .withArguments("-I", "mirakle_init.gradle", "tasks")
+                }
+
+                val buildFile by memoized { folder.newFile("build.gradle.kts") }
+                fun buildFileWriter() = PrintWriter(FileWriter(buildFile, true))
+
+                on("exception occurred on remote side") {
+                    buildFileWriter().use { it.write(THROW) }
+                    val testResult = gradleRunner.testFail()
+
+                    it("should fail") {
+                        testResult.assertBuildFailed()
+                    }
+
+                    it("should execute download task") {
+                        testResult.assertTaskSucceed("downloadFromRemote")
+                    }
+
+                    it("should skip fallback task") {
+                        testResult.assertTaskSkipped("fallback")
+                    }
                 }
             }
         }
