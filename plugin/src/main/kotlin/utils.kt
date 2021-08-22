074195eef87a458e9c2bd0967ea942641aba70eb
adambl4 <dreadambl4@gmail.com>
2021-08-22 11:21:10 +0300

    Use gradlew root when loading properties

--- plugin/src/main/kotlin/utils.kt
+++ plugin/src/main/kotlin/utils.kt
@@ -12,6 +12,7 @@ import org.gradle.api.invocation.Gradle
 import org.gradle.api.tasks.TaskState
 import org.gradle.internal.service.ServiceRegistry
 import java.io.File
+import java.util.*
 
 fun Gradle.logTasks(tasks: List<Task>) = logTasks(*tasks.toTypedArray())
 
